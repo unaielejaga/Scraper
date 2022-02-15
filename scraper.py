f7a28fd7ef0e1ef25c4bd94e4c1885c944b021d8
Unai Elejaga Jimeno <unai.elejaga@opendeusto.es>
2022-02-15 22:47:07 +0100

    Introducir mas webs

--- scraper.py
+++ scraper.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 from bs4 import BeautifulSoup
 import requests
 
@@ -14,48 +13,56 @@ class Receta:
         stringre = "\t"
         for i in self.ingredientes:
             stringre = stringre + '\n\tCantidad: ' + str(i[0][0]) + ', Unidad: ' + str(i[0][1]) + ', Ingrediente: ' + str(i[1])
-        return 'Nombre: ' + str(self.nombre) + '\n' + 'Dificultad: ' + str(self.dificultad) + '\n' + 'Duracion: ' + str(self.duracion) + '\n' + 'Comensales: ' + str(self.comensales) + '\n' + 'Ingredientes: ' + stringre + '\n Descripcion: ' + str(self.descripcion) + '\n'
-        
-web = 'https://www.recetasderechupete.com/quiche-lorraine-receta-de-cocina-francesa/8634/'
-resultado = requests.get(web)
-sopa = BeautifulSoup(resultado.text, 'lxml')
+        return 'Nombre: ' + str(self.nombre) + '\n' + 'Dificultad: ' + str(self.dificultad) + '\n' + 'Duracion: ' + str(self.duracion) + '\n' + 'Comensales: ' + str(self.comensales) + '\n' + 'Ingredientes: ' + stringre + '\nDescripcion: ' + str(self.descripcion) + '\n'
 
-receta = (sopa.find('header').find('h1').text).strip()
 
-spans = [] 
-tag = sopa.find('div', class_='rdr-eat')
-for span in tag.find_all('span', class_='rdr-tag'):
-    spans.append(span.text)
-dificultad = spans[0]
-duracion = spans[1]
-comensales = spans[2]
+f = open('recetas.txt','r')
+with open('recetas.txt') as f:
+    lines = f.readlines()
+f.close()
 
-ingredientes = []
-ingresopa = sopa.find('div', id='ingredients').find('ul')
-for li in ingresopa.find_all('li'):
-    ingre = li.text.split(". ")
-    if len(ingre) == 1:
-        if ingre[0][0].isdigit():
-            ingre = li.text.split(" " , 1)
-            ingre[0] = [ingre[0], 'und']
+for recetaslist in lines:
+    web = recetaslist
+    resultado = requests.get(web)
+    sopa = BeautifulSoup(resultado.text, 'lxml')
+
+    receta = (sopa.find('header').find('h1').text).strip()
+
+    spans = [] 
+    tag = sopa.find('div', class_='rdr-eat')
+    for span in tag.find_all('span', class_='rdr-tag'):
+        spans.append(span.text)
+    dificultad = spans[0]
+    duracion = spans[1]
+    comensales = spans[2]
+
+    ingredientes = []
+    ingresopa = sopa.find('div', id='ingredients').find('ul')
+    for li in ingresopa.find_all('li'):
+        ingre = li.text.split(". ")
+        if len(ingre) == 1:
+            if ingre[0][0].isdigit():
+                ingre = li.text.split(" " , 1)
+                ingre[0] = [ingre[0], 'und']
+            else:
+                ingre = [['1', 'und'], ingre[0]]
         else:
-            ingre = [['1', 'und'], ingre[0]]
-    else:
-        if ingre[0][0].isdigit():
-            ingre[0] = ingre[0].split(" ")
-        else:
-            ingre[0] = (ingre[0].split(": "))[1]
-            ingre[0] = ingre[0].split(" ")
-    ingredientes.append(ingre)
+            if ingre[0][0].isdigit():
+                ingre[0] = ingre[0].split(" ")
+            else:
+                ingre[0] = (ingre[0].split(": "))[1]
+                ingre[0] = ingre[0].split(" ")
+        ingredientes.append(ingre)
 
-descripcion = sopa.find('div', id='description')
-descrip = []
-for ol in descripcion.find_all('ol'):
-    for li in ol.find_all('li'):
-        descrip.append(' '.join(li.text.split("\xa0")))
-descrip = ' '.join(descrip)
+    descripcion = sopa.find('div', id='description')
+    descrip = []
+    for ol in descripcion.find_all('ol'):
+        for li in ol.find_all('li'):
+            descrip.append(' '.join(li.text.split("\xa0")))
+    descrip = ' '.join(descrip)
 
 
-recetafinal = Receta(receta, dificultad, duracion, comensales, ingredientes, descrip)
-print(str(recetafinal))
+    recetafinal = Receta(receta, dificultad, duracion, comensales, ingredientes, descrip)
+    print(str(recetafinal))
+    print("=======================================================================================================")
 
