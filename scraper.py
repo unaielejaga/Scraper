ca482b115681ea7d18b0da62eda45bfdf19352d9
Unai Elejaga Jimeno <unai.elejaga@opendeusto.es>
2022-02-15 16:50:08 +0100

    Falta introducir diccionario a JSON

--- scraper.py
+++ scraper.py
@@ -2,12 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-web = 'https://www.recetasderechupete.com/como-preparar-pollo-teriyaki-de-manera-facil-y-rapida/9848/'
+web = 'https://www.recetasderechupete.com/zaatar-la-mezcla-de-especias-que-dara-un-toque-arabe-a-tus-recetas/44836/'
 resultado = requests.get(web)
 sopa = BeautifulSoup(resultado.text, 'lxml')
-ingredientes = sopa.find('div', id='ingredients').find('ul')
 
-for li in ingredientes.find_all('li'):
+receta = (sopa.find('header').find('h1').text).strip()
+print(receta)
+
+spans = [] 
+tag = sopa.find('div', class_='rdr-eat')
+for span in tag.find_all('span', class_='rdr-tag'):
+    spans.append(span.text)
+dificultad = spans[0]
+duracion = spans[1]
+comensales = spans[2]
+print(dificultad)
+print(duracion)
+print(comensales)
+
+ingredientes = []
+ingresopa = sopa.find('div', id='ingredients').find('ul')
+for li in ingresopa.find_all('li'):
     ingre = li.text.split(". ")
     if len(ingre) == 1:
         if ingre[0][0].isdigit():
@@ -18,4 +33,18 @@ for li in ingredientes.find_all('li'):
     else:
         if ingre[0][0].isdigit():
             ingre[0] = ingre[0].split(" ")
-    print(ingre)
+        else:
+            ingre[0] = (ingre[0].split(": "))[1]
+            ingre[0] = ingre[0].split(" ")
+    ingredientes.append(ingre)
+print(ingredientes)
+
+descripcion = sopa.find('div', id='description')
+
+descrip = []
+for ol in descripcion.find_all('ol'):
+    for li in ol.find_all('li'):
+        descrip.append(' '.join(li.text.split("\xa0")))
+descrip = ' '.join(descrip)
+print(descrip)
+
